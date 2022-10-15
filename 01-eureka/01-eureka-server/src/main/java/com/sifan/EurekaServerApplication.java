07d2dd31092d8e6dbcb369933a35f3aebc319bc9
思凡 <2262427313@qq.com>
2022-10-15 19:21:26 +0800

    格式化代码

--- 01-eureka/01-eureka-server/src/main/java/com/sifan/EurekaServerApplication.java
+++ 01-eureka/01-eureka-server/src/main/java/com/sifan/EurekaServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer   // 开启eureka的注册中心的功能
-public class    EurekaServerApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
